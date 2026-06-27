76d8ac0d9d818cf9ffb15ed801ac299f7100c0af
arsenijzajcev242-blip <arsenijzajcev242@gmail.com>
2026-06-27 15:30:41 +0300

    Update handler.py

--- app/handler.py
+++ app/handler.py
@@ -1,8 +1,8 @@
 from aiogram import types, Router
 from aiogram.filters import CommandStart
-
+import app.keyboard as kb
 router = Router()
 @router.message(CommandStart())
 async def hello_world(message: types.Message):
-    await message.answer(f"Hello, {message.from_user.first_name}!")
+    await message.answer(f"Hello, {message.from_user.first_name}!",reply_markup=kb.main)
 
