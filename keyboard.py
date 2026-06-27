0e72064b8051f4b626ae69b06483c76dbab930e3
arsenijzajcev242-blip <arsenijzajcev242@gmail.com>
2026-06-27 15:30:14 +0300

    Delete keyboard.py

--- keyboard.py
+++
@@ -1,4 +0,0 @@
-from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup)
-main = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='Каталог')],
-                                     [KeyboardButton(text='Профиль'),
-                                     KeyboardButton(text='Баланс')]], resize_keyboard=True)
